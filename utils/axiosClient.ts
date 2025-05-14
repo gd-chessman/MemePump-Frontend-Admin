97a0bd2e9d551806bab79901932ae2ea6baa65c2
Le Van Quy <vannn.quy@gmail.com>
2025-05-14 16:19:08 +0700

    up

--- utils/axiosClient.ts
+++ utils/axiosClient.ts
@@ -21,8 +21,10 @@ axiosClient.interceptors.response.use(
   (error) => {
     if (error.response && error.response.status === 401) {
       console.warn('Lỗi 401: Unauthorized');
-      window.location.href = "/login";
-      localStorage.removeItem('user')
+      if (window.location.pathname !== '/login') {
+        window.location.href = "/login";
+        localStorage.removeItem('user')
+      }
     }else if(error.code === "ERR_NETWORK"){
       console.warn("Máy chủ đang gặp sự cố !");
     }
